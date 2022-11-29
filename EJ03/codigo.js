6935761ee147c0edab88ed251a4219a86efaa070
oscar08850 <oscar08850@gmail.com>
2022-11-29 10:00:31 +0100

    EJ03

---
+++ EJ03/codigo.js
@@ -0,0 +1,6 @@
+//Almacenamos el texto en una variable var
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (const element of meses) {
+    console.log(element);
+}
