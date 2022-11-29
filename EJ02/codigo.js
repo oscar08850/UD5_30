2fed63811d5ae689bc7c017feb34ef44ed92c9e8
oscar08850 <oscar08850@gmail.com>
2022-11-29 10:00:19 +0100

    EJ03

--- EJ02/codigo.js
+++ EJ02/codigo.js
@@ -1,5 +1,5 @@
 //Almacenamos el texto en una variable var
-var mensaje = "Hola Mundo!\n Que fácil es poner 'comillas simples' y \"comillas dobles\"";
+var mensaje = "Hola Mundo!\nQue fácil es poner 'comillas simples' y \"comillas dobles\"";
 
 //Invocamos un alert para mostrar al usuario.
 alert(mensaje);
